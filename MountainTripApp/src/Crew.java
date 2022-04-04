68f1fe3d2a551b05459fc9a69a7203ac7c2af574
Tomasz <tomaszboron2k@gmail.com>
2022-04-04 17:44:56 +0200

    get response from supplier

--- MountainTripApp/src/Crew.java
+++ MountainTripApp/src/Crew.java
@@ -1,7 +1,4 @@
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -17,7 +14,6 @@ public class Crew {
     public Crew() throws IOException, TimeoutException {
 
         System.out.println("CREW");
-
         System.out.println("Put your name here: ");
         name = scanner.nextLine();
 
@@ -25,20 +21,42 @@ public class Crew {
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
         channel.exchangeDeclare(exchange_name, BuiltinExchangeType.TOPIC);
 
-        getOrders(channel);
+        listenSupplierResponses(channel);
+
+        makeOrders(channel);
     }
 
-    private void getOrders(Channel channel) throws IOException {
+    private void makeOrders(Channel channel) throws IOException {
         while(true){
             System.out.println("Put your order here:");
             String requestedItem = scanner.nextLine();
+            if(requestedItem.equals("exit")) break;
             String key = name + "." + requestedItem.toLowerCase();
             String message = name;
             channel.basicPublish(exchange_name, key, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println("Sent: " + message + " order " + requestedItem);
         }
+    }
+
+    private void listenSupplierResponses(Channel channel) throws IOException {
+        String queueName = name+"_orders";
+        channel.queueDeclare(queueName, false, false, false, null);
+        String key = "order."+name;
+        channel.queueBind(queueName, exchange_name, key);
+        System.out.println("Initialize queue for responses from suppliers with name: " + queueName);
+        handleResponse(channel, queueName);
+    }
+
+    private void handleResponse(Channel channel, String queueName) throws IOException {
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println("Received: " + message);
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
     }
 }
--- MountainTripApp/src/Supplier.java
+++ MountainTripApp/src/Supplier.java
@@ -3,6 +3,7 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
@@ -16,28 +17,24 @@ public class Supplier {
     public Supplier() throws IOException, TimeoutException {
 
         System.out.println("SUPPLIER");
-
         System.out.println("Put your name here: ");
         name = scanner.nextLine();
-
-        products = new ArrayList<>();
-
-        defineItems();
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
         channel.exchangeDeclare(exchange_name, BuiltinExchangeType.TOPIC);
 
+        products = new ArrayList<>();
+        defineItems();
         registerProducts(channel);
 
     }
 
     private void defineItems(){
         System.out.println("Put the list of available items");
-        System.out.println("Writing submit ends the list");
+        System.out.println("Writing 'submit' ends the list");
         while(true){
             String product = scanner.nextLine();
             if(product.equals("submit")) break;
@@ -50,18 +47,23 @@ public class Supplier {
             channel.queueDeclare(product, false, false, false, null);
             String key = "*." + product;
             channel.queueBind(product, exchange_name, key);
-            System.out.println("Created product queue: " + product);
-            handleMessage(channel, product);
+            getOrders(channel, product);
         }
     }
 
-    private void handleMessage(Channel channel, String queueName) throws IOException {
+    private void getOrders(Channel channel, String queueName) throws IOException {
         Consumer consumer = new DefaultConsumer(channel){
             @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, StandardCharsets.UTF_8);
                 String item = getItemType(envelope.getRoutingKey());
                 System.out.println("Received: " + message + " want " + item);
+
+                /// response
+                String key = "order."+message;
+                String response = "Supplier " + name + " realised order: " + item + " with id: " + generateOrderID();
+                channel.basicPublish(exchange_name, key, null, response.getBytes(StandardCharsets.UTF_8));
+                System.out.println("Sent: " + response + " to " + message);
             }
         };
 
@@ -70,5 +72,9 @@ public class Supplier {
 
     private String getItemType(String key){
         return key.split("\\.")[1];
+    }
+
+    private int generateOrderID(){
+        return new Random().nextInt(1, 999999);
     }
 }
